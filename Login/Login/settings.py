c81d77fbc5a0308ccb81f16ff84deed917bcc95d
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2021-09-22 08:59:41 -0300

    Update settings.py

--- Login/Login/settings.py
+++ Login/Login/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fernandoheredia33@gmail.com'
-EMAIL_HOST_PASSWORD = 'Rbcarp01'
+EMAIL_HOST_USER = 'xxxxxxxxxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxx'
 
 
 
